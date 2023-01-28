d86cf241f250ddb5436aab37a3e729803c7f13c1
Mohammed-Am <mh.amouzoun@gmail.com>
2023-01-28 15:43:07 +0100

    add icons in  custom app barr

---
+++ lib/views/custom_app_bar.dart
@@ -0,0 +1,22 @@
+import 'package:block_nots/views/custom_icon.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          'Notes',
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
