cfbe36002182b7c28c2b7131086bfbba7a47eabe
Mohammed-Am <mh.amouzoun@gmail.com>
2023-01-31 21:59:11 +0100

    add border raidus to buttom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,3 +1,4 @@
+import 'package:block_nots/views/add_note_bottom_sheet.dart';
 import 'package:block_nots/views/notes_view_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -9,8 +10,19 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton:
-          FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              context: context,
+              builder: (context) {
+                return const AddNoteButtomSheet();
+              });
+        },
+        child: const Icon(Icons.add),
+      ),
       body: const NotesViewBody(),
     );
   }
